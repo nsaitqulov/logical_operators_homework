18591bbdf7b35d94d6b3a1adafb8ea0620044eb6
Nurbek <nurbek.saitqulov@gmail.com>
2022-11-19 12:48:49 +0500

    logic16.py

--- logic16.py
+++ logic16.py
@@ -6,4 +6,15 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x=a%10
+    a//=10
+    x1=a%10
+    a//=10
+    x2=a%10
+    a//=10
+    x3=a%10
+    a//=10
+    x4=a%10
+
+    return x4!=0 and x3*1000+x2*100+x1*10+x!=a
+print(main(763))
